940912b309f8d9f6b6df97aaac2d2a2178926f92
SeymaDemir <seymademir35@gmail.com>
2023-02-11 15:31:06 +0300

    edited buttons

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -29,14 +29,23 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
+                <div className="flex flex-row justify-between pl-16 pr-16">
                 <a href="https://main--jocular-kitsune-979dd9.netlify.app/">
                   <button
                     type="button"
                     class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   >
-                    See Demo
+                    DEMO
                   </button>
                 </a>
+                <a href="https://github.com/Recoded-Spark-Turkey-2022/RE-Minded">
+                  <button
+                    type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    CODE
+                  </button>
+                </a></div>
               </div>
             </div>
           </div>
@@ -52,14 +61,23 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
+                <div className="flex flex-row justify-between pl-16 pr-16">
                 <a href="https://effervescent-longma-4868ea.netlify.app/">
                   <button
                     type="button"
                     class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   >
-                    See Demo
+                    DEMO
                   </button>
                 </a>
+                <a href="https://github.com/symys/todoapp">
+                  <button
+                    type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    CODE
+                  </button>
+                </a></div>
               </div>
             </div>
           </div>
@@ -75,14 +93,23 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
+                <div className="flex flex-row justify-between pl-16 pr-16">
                 <a href="https://nimble-wisp-86e992.netlify.app/">
                   <button
                     type="button"
                     class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   >
-                    See Demo
+                    DEMO
                   </button>
                 </a>
+                <a href="https://github.com/symys/advice-app">
+                  <button
+                    type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    CODE
+                  </button>
+                </a></div>
               </div>
             </div>
           </div>
@@ -100,14 +127,25 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
+                <div className="flex flex-row justify-between pl-16 pr-16">
                 <a href="https://recoded-spark-turkey-2022.github.io/movie-project-playing-our-way/">
                   <button
                     type="button"
                     class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                   >
-                    See Demo
+                    DEMO
                   </button>
+                  
                 </a>
+                <a href="https://github.com/Recoded-Spark-Turkey-2022/movie-project-playing-our-way">
+                  <button
+                    type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    CODE
+                  </button>
+                  
+                </a></div>
               </div>
             </div>
           </div>
