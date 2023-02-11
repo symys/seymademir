d71da749ef2b6546d56628eac3173f91a1c18239
SeymaDemir <seymademir35@gmail.com>
2023-02-11 15:20:02 +0300

    edited buttons

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,26 +1,26 @@
 import React from "react";
-import adviceImg from "../images/advice.JPG"
-import todoImg from "../images/todo.JPG"
-import healingImg from "../images/healing.JPG"
-import movieImg from "../images/movie.JPG"
-import madlibsImg from "../images/madlibs.JPG"
-
+import adviceImg from "../images/advice.JPG";
+import todoImg from "../images/todo.JPG";
+import healingImg from "../images/healing.JPG";
+import movieImg from "../images/movie.JPG";
+import madlibsImg from "../images/madlibs.JPG";
 
 function Projects() {
   return (
-    <div className="pt-20 font-rubik"  id="latestprojects">
-      <h1 className="tracking-wider text-4xl flex justify-center mt-16">Latest Projects</h1>
+    <div className="pt-20 font-rubik" id="latestprojects">
+      <h1 className="tracking-wider text-4xl flex justify-center mt-16">
+        Latest Projects
+      </h1>
       <div className="flex flex-col mt-16">
         <div className="flex flex-row justify-between pl-20 pr-20">
           <div class="flex ">
             <div class="rounded-lg shadow-lg bg-white max-w-sm">
-              <a href="https://main--jocular-kitsune-979dd9.netlify.app/">
-                <img
-                  class="rounded-t-lg"
-                  src={healingImg}
-                  alt="adviceProject_image"
-                />
-              </a>
+              <img
+                class="rounded-t-lg"
+                src={healingImg}
+                alt="adviceProject_image"
+              />
+
               <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">
                   Online Therapist
@@ -29,24 +29,21 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <button
-                  type="button"
-                  class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                >
-                  Button
-                </button>
+                <a href="https://main--jocular-kitsune-979dd9.netlify.app/">
+                  <button
+                    type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    See Demo
+                  </button>
+                </a>
               </div>
             </div>
           </div>
           <div class="flex ">
             <div class="rounded-lg shadow-lg bg-white max-w-sm">
-              <a href="https://effervescent-longma-4868ea.netlify.app/">
-                <img
-                  class="rounded-t-lg"
-                  src={todoImg}
-                  alt=""
-                />
-              </a>
+              <img class="rounded-t-lg" src={todoImg} alt="" />
+
               <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">
                   To-Do App
@@ -55,24 +52,21 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <button
-                  type="button"
-                  class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                >
-                  Button
-                </button>
+                <a href="https://effervescent-longma-4868ea.netlify.app/">
+                  <button
+                    type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    See Demo
+                  </button>
+                </a>
               </div>
             </div>
           </div>
           <div class="flex ">
             <div class="rounded-lg shadow-lg bg-white max-w-sm">
-              <a href="https://nimble-wisp-86e992.netlify.app/">
-                <img
-                  class="rounded-t-lg"
-                  src={adviceImg}
-                  alt=""
-                />
-              </a>
+              <img class="rounded-t-lg" src={adviceImg} alt="" />
+
               <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">
                   Random Advice App
@@ -81,12 +75,14 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <button
-                  type="button"
-                  class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                >
-                  Button
-                </button>
+                <a href="https://nimble-wisp-86e992.netlify.app/">
+                  <button
+                    type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    See Demo
+                  </button>
+                </a>
               </div>
             </div>
           </div>
@@ -94,13 +90,8 @@ function Projects() {
         <div className="flex flex-row justify-between pl-20 pr-20 pt-16">
           <div class="flex ">
             <div class="rounded-lg shadow-lg bg-white max-w-sm">
-              <a href="https://recoded-spark-turkey-2022.github.io/movie-project-playing-our-way/">
-                <img
-                  class="rounded-t-lg"
-                  src={movieImg}
-                  alt=""
-                />
-              </a>
+              <img class="rounded-t-lg" src={movieImg} alt="" />
+
               <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">
                   Movie Project
@@ -109,24 +100,21 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <button
-                  type="button"
-                  class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                >
-                  Button
-                </button>
+                <a href="https://recoded-spark-turkey-2022.github.io/movie-project-playing-our-way/">
+                  <button
+                    type="button"
+                    class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                  >
+                    See Demo
+                  </button>
+                </a>
               </div>
             </div>
           </div>
           <div class="flex ">
             <div class="rounded-lg shadow-lg bg-white max-w-sm">
-              <a href="https://recoded-spark-turkey-2022.github.io/mad-libs-los-galacticos/">
-                <img
-                  class="rounded-t-lg"
-                  src={madlibsImg}
-                  alt=""
-                />
-              </a>
+              <img class="rounded-t-lg" src={madlibsImg} alt="" />
+
               <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">
                   Mad Libs Game
@@ -135,24 +123,31 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <button
-                  type="button"
-                  class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                >
-                  Button
-                </button>
+                <div className="flex flex-row justify-between pl-16 pr-16">
+                  <a href="https://recoded-spark-turkey-2022.github.io/mad-libs-los-galacticos/">
+                    <button
+                      type="button"
+                      class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                    >
+                      DEMO
+                    </button>
+                  </a>
+                  <a href="https://github.com/Recoded-Spark-Turkey-2022/mad-libs-los-galacticos">
+                    <button
+                      type="button"
+                      class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                    >
+                      CODE
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
           <div class="flex ">
             <div class="rounded-lg shadow-lg bg-white max-w-sm">
-              <a href="https://recoded-spark-turkey-2022.github.io/mad-libs-los-galacticos/">
-                <img
-                  class="rounded-t-lg"
-                  src={madlibsImg}
-                  alt=""
-                />
-              </a>
+              <img class="rounded-t-lg" src={madlibsImg} alt="" />
+
               <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2">
                   Mad Libs Game
@@ -161,12 +156,24 @@ function Projects() {
                   Some quick example text to build on the card title and make up
                   the bulk of the card's content.
                 </p>
-                <button
-                  type="button"
-                  class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                >
-                  Button
-                </button>
+                <div className="flex flex-row justify-between pl-16 pr-16">
+                  <a href="https://recoded-spark-turkey-2022.github.io/mad-libs-los-galacticos/">
+                    <button
+                      type="button"
+                      class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                    >
+                      DEMO
+                    </button>
+                  </a>
+                  <a href="https://github.com/Recoded-Spark-Turkey-2022/mad-libs-los-galacticos">
+                    <button
+                      type="button"
+                      class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                    >
+                      CODE
+                    </button>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
