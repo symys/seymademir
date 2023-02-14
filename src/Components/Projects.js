4dc21aeeca9accbfed44baad5a191b8ed8d17736
SeymaDemir <seymademir35@gmail.com>
2023-02-14 19:12:01 +0300

    added projects descriptions

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -4,6 +4,7 @@ import todoImg from "../images/todo.JPG";
 import healingImg from "../images/healing.JPG";
 import movieImg from "../images/movie.JPG";
 import madlibsImg from "../images/madlibs.JPG";
+import minionsImg from "../images/minions.JPG"
 
 function Projects() {
   return (
@@ -25,11 +26,10 @@ function Projects() {
                 <h5 class="text-gray-900 text-xl font-medium mb-2 justify-center flex">
                   Online Therapist
                 </h5>
-                {/* <p class="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p> */}
-                <div className="flex flex-row justify-between pl-16 pr-16 pt-12">
+                <p class="text-gray-700 text-base mb-4 text-sm">
+                This project is the capstone project of Recoded bootcamp and it was developed with a group of 5 people. The draft of the project was given in Figma and consisted of 30 pages. We created a responsive online therapist site in 7 weeks using React, Firebase, Redux, JEST, Tailwind and necessary technologies. The project can be experienced live on Netlify.
+                </p> 
+                <div className="flex flex-row justify-between pl-16 pr-16 pt-28">
                 <a href="https://main--jocular-kitsune-979dd9.netlify.app/">
                   <button
                     type="button"
@@ -57,10 +57,9 @@ function Projects() {
                 <h5 class="text-gray-900 text-xl font-medium mb-2 justify-center flex">
                   To-Do App
                 </h5>
-                {/* <p class="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p> */}
+                <p class="text-gray-700 text-base mb-4 text-sm">
+                I aimed to learn React and Redux usage in depth with this project. Used technologies are React, Redux and Tailwind. The tasks are kept in 4 different categories. categories are taken from Eisenhover matrix and users can filter tasks by categories. They can add a new task or delete a task. For those who want to learn the Eisenhover matrix in detail, there is a "See Groups" button on the main page and it leads to a page describing the matrix. In addition, summary information has been added to the category filtering buttons in the section where the tasks are added.
+                </p> 
                 <div className="flex flex-row justify-between pl-16 pr-16 pt-12">
                 <a href="https://effervescent-longma-4868ea.netlify.app/">
                   <button
@@ -89,11 +88,10 @@ function Projects() {
                 <h5 class="text-gray-900 text-xl font-medium mb-2 justify-center flex">
                   Random Advice App
                 </h5>
-                {/* <p class="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p> */}
-                <div className="flex flex-row justify-between pl-16 pr-16 pt-12">
+                 <p class="text-gray-700 text-base mb-4 text-sm">
+                 A challenge from Frontend Mentor is to build out this advice generator app using the Advice Slip API and get it looking as close to the design as possible. Used technologies are HTML, CSS and JavaScript. It gets random advice from the API with fetch, and the advice changes every time when users click the dice. It was designed to make it compatible with different devices by making a responsive design. 
+                </p> 
+                <div className="flex flex-row justify-between pl-16 pr-16 pt-28">
                 <a href="https://nimble-wisp-86e992.netlify.app/">
                   <button
                     type="button"
@@ -123,10 +121,9 @@ function Projects() {
                 <h5 class="text-gray-900 text-xl font-medium mb-2 justify-center flex">
                   Movie Project
                 </h5>
-                {/* <p class="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p> */}
+                 <p class="text-gray-700 text-base mb-4 text-sm">
+                 It is a Re:Coded group project with 4 people. It aims to teach DOM manipulation deeply and get data with fetching in JS. Used technologies are JS, HTML, CSS. There is a welcome page at the entrance. In the home page users can see popular films from The Movie Database API. Films can be listed according to their genres, popularity, rated. In addition to these users can list actors. Every actor has an actor detail page which displays biography and films of actor. Like actor detail page, every film has film detail page which contains film details, overview, production companies, actors, trailer and similar films.
+                </p> 
                 <div className="flex flex-row justify-between pl-16 pr-16 pt-12">
                 <a href="https://recoded-spark-turkey-2022.github.io/movie-project-playing-our-way/">
                   <button
@@ -157,11 +154,10 @@ function Projects() {
                 <h5 class="text-gray-900 text-xl font-medium mb-2 justify-center flex">
                   Mad Libs Game
                 </h5>
-                {/* <p class="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p> */}
-                <div className="flex flex-row justify-between pl-16 pr-16 pt-12">
+                 <p class="text-gray-700 text-base mb-4 text-sm">
+                  It is a Re:Coded group project. Used technologies are HTML, CSS and JS. The logic is to complete the given story with filling the blanks to your imagination. When you open the project page you will see 2 same stories with blanks. You can write your own story with editing left paragraph. During editing proccess you will see the changes in the right paragraph synchronously. If you want to reset the game you should just click the “Reset” button top of the editing part.
+                </p> 
+                <div className="flex flex-row justify-between pl-16 pr-16 pt-28">
                   <a href="https://recoded-spark-turkey-2022.github.io/mad-libs-los-galacticos/">
                     <button
                       type="button"
@@ -184,18 +180,17 @@ function Projects() {
           </div>
           <div class="flex lg:pt-0 pt-10">
             <div class="rounded-lg shadow-lg bg-white max-w-sm">
-              <img class="rounded-t-lg" src={madlibsImg} alt="" />
+              <img class="rounded-t-lg" src={minionsImg} alt="" />
 
               <div class="p-6">
                 <h5 class="text-gray-900 text-xl font-medium mb-2 justify-center flex">
-                  Mad Libs Game
+                  Minion Toys Shop
                 </h5>
-                {/* <p class="text-gray-700 text-base mb-4">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p> */}
-                <div className="flex flex-row justify-between pl-16 pr-16 pt-12">
-                  <a href="https://recoded-spark-turkey-2022.github.io/mad-libs-los-galacticos/">
+                <p class="text-gray-700 text-base mb-4 justify-center flex text-sm">
+                  *Currently I'm working on this project using Angular
+                </p> 
+                <div className="flex flex-row justify-between pl-16 pr-16 pt-72">
+                  <a href="https://roaring-khapse-667b6e.netlify.app/">
                     <button
                       type="button"
                       class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
@@ -203,7 +198,7 @@ function Projects() {
                       DEMO
                     </button>
                   </a>
-                  <a href="https://github.com/Recoded-Spark-Turkey-2022/mad-libs-los-galacticos">
+                  <a href="https://github.com/symys/minions">
                     <button
                       type="button"
                       class=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
